58aa225bd37c3adfdfaa2bb36b89497e65d7c3f9
Jonathan Birkholz <rookieone@gmail.com>
2016-07-12 10:24:18 -0500

    display reddit posts

--- src/posts.ts
+++ src/posts.ts
@@ -2,8 +2,13 @@ import app from './app'
 
 export default function() {
   return app.app.component('posts', {
-    controller: function($scope) {
+    controller: function($scope, $http) {
       $scope.message = "Hello Posts"
+      $http.get("http://www.reddit.com/r/pics/search.json?q=kittens&sort=new")
+        .then(function(response) {
+          console.log("result", response.data.data.children)
+          $scope.posts = response.data.data.children
+        })
     },
     templateUrl: 'templates/posts.html'
   })
