b8b2c3d20a7e84334f96ae9ba2c68737a11ded35
Jonathan Birkholz <rookieone@gmail.com>
2016-07-12 09:11:05 -0500

    webpack and typescript setup

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+document.body.innerHTML = "Hello Webpack & Typescript"
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path')
+
+module.exports = {
+  entry: [
+    './src/index.ts'
+  ],
+
+  output: {
+      path: __dirname,
+      filename: "bundle.js"
+  },
+
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['', '.ts', '.tsx', '.js']
+  },
+
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ],
+  },
+
+  modulesDirectories: [
+    'node_modules',
+  ],
+}
